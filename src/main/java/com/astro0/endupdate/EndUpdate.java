21479eb7a01ee16aab74c1f0710cead6b8c6c193
astro367 <78714325+astro367@users.noreply.github.com>
2021-07-21 14:52:46 +1200

    Initial commit

---
+++ src/main/java/com/astro0/endupdate/EndUpdate.java
@@ -0,0 +1,30 @@
+package com.astro0.endupdate;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("endup")
+public class EndUpdate
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+
+    public EndUpdate() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
